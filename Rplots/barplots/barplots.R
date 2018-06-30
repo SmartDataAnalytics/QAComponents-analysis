470ecdbf5cd4b878e51610bd7e9907135c63c3f9
Ioanna Lytra <ilytra@localhost.localdomain>
2018-06-30 17:20:03 +0200

    add frequencies of features

--- Rplots/barplots/barplots.R
+++ Rplots/barplots/barplots.R
@@ -21,7 +21,8 @@ library(ggplot2)
 ### functions
 #########################################################
 
-plotbars1 <- function(data, ylab, title, fill) {
+# single barplot for each QA task (unanswered questions)
+plotUnansweredPerTask <- function(data, ylab, title, fill) {
   p <- ggplot(data = data,
               aes(x = feature, y = number)) +
     xlab(NULL) +
@@ -36,7 +37,8 @@ plotbars1 <- function(data, ylab, title, fill) {
   return(p)
 }
 
-plotbars2 <- function(data, xlab, ylab) {
+# barplot for all QA tasks (unanswered questions)
+plotAllUnanswered <- function(data, xlab, ylab) {
   p <- ggplot(data = data,
               aes(x = feature, y = number)) +
     xlab(xlab) +
@@ -53,6 +55,24 @@ plotbars2 <- function(data, xlab, ylab) {
   return(p)
 }
 
+# barplot frequency of features
+plotFrequencies <- function(data, xlab, ylab) {
+  p <- ggplot(data = data,
+              aes(x = feature, y = questions)) +
+    xlab(xlab) +
+    ylab(ylab) +
+    theme_minimal() +
+    geom_bar(stat = "identity",
+             aes(fill = factor(task))) +
+    scale_fill_manual("legend", values = c("NED" = "#817f82", "RL" = "#b89685", "CL" = "#bcabae", "QB" = "#322214")) +
+    theme(legend.position = "top",
+          legend.title = element_blank(),
+          legend.text = element_text(size = 22),
+          axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 20),
+          axis.text.y = element_text(size = 20),
+          axis.title.y = element_text(size = 22))
+  return(p)
+}
 
 #########################################################
 ### plot all barplots
@@ -60,23 +80,23 @@ plotbars2 <- function(data, xlab, ylab) {
 
 # Unanswered questions per question type
 neddata <- read.csv("data/ned-unanswered.csv", sep = ',')
-plot1 <- plotbars1(neddata, "# of Questions", "NED", "#817f82") +
+plot1 <- plotUnansweredPerTask(neddata, "# of Questions", "NED", "#817f82") +
   scale_y_continuous(limits = c(0, 900),
                      breaks = c(50, 150, 300, 450, 600, 750))
 
 reldata <- read.csv("data/rel-unanswered.csv", sep = ',')
-plot2 <- plotbars1(reldata, "# of Questions", "RL", "#b89685") +
+plot2 <- plotUnansweredPerTask(reldata, "# of Questions", "RL", "#b89685") +
   scale_y_continuous(limits = c(0, 900),
                      breaks = c(250, 500, 750))
 
 classdata <- read.csv("data/class-unanswered.csv", sep = ',')
-plot3 <- plotbars1(classdata, "", "CL", "#bcabae") +
+plot3 <- plotUnansweredPerTask(classdata, "", "CL", "#bcabae") +
   scale_y_continuous(limits = c(0, 900),
                      breaks = c(250, 500, 750)) +
   theme(axis.text.x = element_text(margin = margin(t = 0, r = 0, b = 25, l = 0)))
 
 qbdata <- read.csv("data/qb-unanswered.csv", sep = ',')
-plot4 <- plotbars1(qbdata, "", "QB", "#322214") +
+plot4 <- plotUnansweredPerTask(qbdata, "", "QB", "#322214") +
   scale_y_continuous(limits = c(0, 900),
                      breaks = c(250, 500, 750))
 
@@ -84,7 +104,6 @@ pdf("unanswered-1.pdf")
 lay <- rbind(c(1,1,1,1,1),
              c(2,2,3,4,4))
 grid.arrange(plot1, plot2, plot3, plot4, layout_matrix = lay, heights=c(7,4.5))
-dev.off()
 
 # All unanswered questions
 allunanswered <- read.csv("data/unanswered.csv", sep = ',')
@@ -94,7 +113,34 @@ allunanswered$feature <- factor(allunanswered$feature,
                                     "CL", 
                                     "QB"),
                          ordered = TRUE)
-plot5 <- plotbars2(allunanswered, "QA Component Type", "# of Questions")
+plot5 <- plotAllUnanswered(allunanswered, "QA Component Type", "# of Questions")
 
 ggsave("unanswered-2.pdf", plot5)
 
+# Frequency of features along the questions
+frequencies <- read.csv("data/question-features.csv", sep = ',')
+frequencies$feature <- factor(frequencies$feature,
+                              levels = c("e=1", "e=1, caps", "e=1, no caps", "e=1, explicit", "e=1, implicit",
+                                         "e=1, special char", "e=1, no special char", "e=1, words=1 or 1.5",
+                                         "e=1, words=2 or 2.5", "e=1, words=3 or 3.5", "e=1, words=4 or 4.5",
+                                         "e=1, words=5 or more", "e=2", "e=2, no caps", "e=2, one in caps",
+                                         "e=2, all in caps", "e=2, explicit", "e=2, one explicit",
+                                         "e=2, implicit", "e=2, no special char", "e=2, one with special char",
+                                         "e=2, special char", "e=2, words=1 or 1.5", "e=2, words=2 or 2.5",
+                                         "e=2, words=3 or 3.5", "e=2, words=4 or 4.5", "rel=1", "rel=2",
+                                         "rel=1, explicit", "rel=1, implicit", "rel=1, present",
+                                         "rel=1, not present", "rel=1, wc=1", "rel=1, wc=2", "rel=1, wc>2",
+                                         "rel=2, 0 hidden", "rel=2, 1 hidden", "rel=2, 2 hidden",
+                                         "rel=2, 0 explicit", "rel=2, 1 explicit", "rel=2, 2 explicit",
+                                         "rel=2, av. wc=0.5", "rel=2, av. wc=1", "rel=2, av. wc=1.5", "rel=2, av. wc=2",
+                                         "implicit class", "explicit class", "wc=0 or 1", "wc=2", "wc=3",
+                                         "triple size=2", "triple size=3", "triple size=4", "resources=1",
+                                         "resources=2", "relations=1", "relations=2", "classes=0", "classes=1",
+                                         "answer type=list", "answer type=count", "answer type=boolean"
+                              ),
+                              ordered = TRUE)
+plot6 <- plotFrequencies(frequencies, "", "# of Questions")
+
+ggsave("feature-frequencies.pdf", plot6, height = 10, width = 20)
+
+dev.off()
